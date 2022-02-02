6e6ccadf14b5b786642a15200bef2fac7e9118cf
savrasovmv <savrasovmv@tmenergo.ru>
2022-02-02 10:16:49 +0500

    add base hr doc

--- addons/hr_adbook/models/hr_personal_doc.py
+++ addons/hr_adbook/models/hr_personal_doc.py
@@ -1,25 +1,17 @@
 from odoo import fields, models, api
 
-class HrRecruitmentDoc(models.Model):
-    _name = "hr.recruitment_doc"
-    _description = "Прием на работу"
-    _order = "date desc"
+class BaseHrDoc(models.AbstractModel):
+    _name = 'hr.base_doc'
+    _description = 'Базовая модель для персональных докуменов HR'
 
     name = fields.Char(u'Наименование', compute='_get_name', store=True )
     date = fields.Date(string='Дата документа')
-    service_start_date = fields.Date(string='Дата приема', help="Дата с которой сотрудник устроен на работу (первый рабочий день) и исполняет обязанности по должности")
-
     guid_1c = fields.Char(string='guid1C', readonly=True, groups="base.group_erp_manager, base.group_system")
     number_1c = fields.Char(string='Код 1С', readonly=True)
     employee_guid_1c = fields.Char(string='guid сотрудника 1C', readonly=True)
-    employment_type = fields.Char(string='Вид занятости', readonly=True)
-    job_title = fields.Char(string='Должность', readonly=True)
-    department_id = fields.Many2one("hr.department", string="Подразделение")
-    department_guid_1c = fields.Char(string="guid подразделение 1C")
     posted = fields.Boolean(string='Проведен?')
 
-    employee_id = fields.Many2one("hr.employee", string="Сотрудник HR")
-
+    employee_id = fields.Many2one("hr.employee", string="Сотрудник HR", compute="_get_employee", store=True)
 
     @api.depends("employee_id")
     def _get_name(self):
@@ -27,26 +19,54 @@ class HrRecruitmentDoc(models.Model):
             if line.employee_id:
                 line.name = line.employee_id.name
 
-    # @api.model
-    # def create(self, vals):
-    #     doc = super(HrRecruitmentDoc, self).create(vals)
-    #     if vals.get('posted'):
-    #         self.env['sync.tasks'].sudo().create_task(self)
-    #     return doc
+    def _get_employee(self):
+        for line in self:
+            if line.employee_guid_1c:
+                empl_search = self.env['hr.employee'].search([
+                    ('guid_1c', '=', line.employee_guid_1c),
+                    '|',
+                    ('active', '=', False), 
+                    ('active', '=', True)
+                    ],limit=1)
+                line.employee_id = empl_search.id
+            else:
+                line.employee_id = False
+
+
+
+class HrRecruitmentDoc(models.Model):
+    _name = "hr.recruitment_doc"
+    _inherit = 'hr.base_doc'
+    _description = "Прием на работу"
+    _order = "date desc"
     
-    # def write(self, vals):
-    #     doc = super(HrRecruitmentDoc, self).write(vals)
-    #     self.env['sync.tasks'].sudo().update_task(self)
-    #     return doc
+    service_start_date = fields.Date(string='Дата приема', help="Дата с которой сотрудник устроен на работу (первый рабочий день) и исполняет обязанности по должности")
+    employment_type = fields.Char(string='Вид занятости', readonly=True)
+    job_title = fields.Char(string='Должность', readonly=True)
+    department_id = fields.Many2one("hr.department", string="Подразделение", compute="_get_department", store=True)
+    department_guid_1c = fields.Char(string="guid подразделение 1C")
+
+    def _get_department(self):
+        for line in self:
+            if line.department_guid_1c:
+                dep_search = self.env['hr.department'].search([
+                    ('guid_1c', '=', line.department_guid_1c),
+                    '|',
+                    ('active', '=', False), 
+                    ('active', '=', True)
+                    ],limit=1)
+                line.department_id = dep_search.id
+            else:
+                line.department_id = False
+
 
 
 class HrTerminationDoc(models.Model):
     _name = "hr.termination_doc"
+    _inherit = 'hr.base_doc'
     _description = "Увольнения"
     _order = "date desc"
 
-    name = fields.Char(u'Наименование', compute='_get_name', store=True )
-    date = fields.Date(string='Дата документа', readonly=True)
     service_termination_date = fields.Date(
         string="Дата увольнения",
         help=(
@@ -54,148 +74,78 @@ class HrTerminationDoc(models.Model):
         ),
     )
 
-    guid_1c = fields.Char(string='guid1C', readonly=True, groups="base.group_erp_manager, base.group_system")
-    number_1c = fields.Char(string='Код 1С', readonly=True)
-    employee_guid_1c = fields.Char(string='guid сотрудника 1C', readonly=True)
-    posted = fields.Boolean(string='Проведен?')
-
-    employee_id = fields.Many2one("hr.employee", string="Сотрудник HR", readonly=True)
-
-    @api.depends("employee_id")
-    def _get_name(self):
-        for line in self:
-            if line.employee_id:
-                line.name = line.employee_id.name
-
-    # @api.model
-    # def create(self, vals):
-    #     doc = super(HrTerminationDoc, self).create(vals)
-    #     if vals.get('posted'):
-    #         self.env['sync.tasks'].sudo().create_task(self)
-    #     return doc
-    
-    # def write(self, vals):
-    #     doc = super(HrTerminationDoc, self).write(vals)
-    #     self.env['sync.tasks'].sudo().update_task(self)
-    #     return doc
-
-    # def write(self, vals):
-    #     result = super(Survey, self).write(vals)
-    #     if 'certification_give_badge' in vals:
-    #         return self.sudo()._handle_certification_badges(vals)
-    #     return result
-
-    
 
 
 
 class HrVacationDoc(models.Model):
     _name = "hr.vacation_doc"
+    _inherit = 'hr.base_doc'
     _description = "Отпуска"
     _order = "date desc"
 
-    name = fields.Char(u'Наименование', compute='_get_name', store=True )
-    date = fields.Date(string='Дата документа', readonly=True)
     start_date = fields.Date(string="Начало")
     end_date = fields.Date(string="Окончание")
 
-    guid_1c = fields.Char(string='guid1C', readonly=True, groups="base.group_erp_manager, base.group_system")
-    number_1c = fields.Char(string='Код 1С', readonly=True)
-    employee_guid_1c = fields.Char(string='guid сотрудника 1C', readonly=True)
-    posted = fields.Boolean(string='Проведен?', readonly=True)
-
-    employee_id = fields.Many2one("hr.employee", string="Сотрудник HR", readonly=True)
-
-    @api.depends("employee_id")
-    def _get_name(self):
-        for line in self:
-            if line.employee_id:
-                line.name = line.employee_id.name
 
 
 class HrTripDoc(models.Model):
     _name = "hr.trip_doc"
+    _inherit = 'hr.base_doc'
     _description = "Командировки"
     _order = "date desc"
 
-    name = fields.Char(u'Наименование', compute='_get_name', store=True )
-    date = fields.Date(string='Дата документа', readonly=True)
     start_date = fields.Date(string="Начало")
     end_date = fields.Date(string="Окончание")
 
-    guid_1c = fields.Char(string='guid1C', readonly=True, groups="base.group_erp_manager, base.group_system")
-    number_1c = fields.Char(string='Код 1С', readonly=True)
-    employee_guid_1c = fields.Char(string='guid сотрудника 1C', readonly=True)
-    posted = fields.Boolean(string='Проведен?', readonly=True)
-
-    employee_id = fields.Many2one("hr.employee", string="Сотрудник HR", readonly=True)
-
-    @api.depends("employee_id")
-    def _get_name(self):
-        for line in self:
-            if line.employee_id:
-                line.name = line.employee_id.name
 
 
 
 class HrSickLeaveDoc(models.Model):
     _name = "hr.sick_leave_doc"
+    _inherit = 'hr.base_doc'
     _description = "Больничные"
     _order = "date desc"
 
-    name = fields.Char(u'Наименование', compute='_get_name', store=True )
-    date = fields.Date(string='Дата документа', readonly=True)
     start_date = fields.Date(string="Начало")
     end_date = fields.Date(string="Окончание")
-
-    guid_1c = fields.Char(string='guid1C', readonly=True, groups="base.group_erp_manager, base.group_system")
-    number_1c = fields.Char(string='Код 1С', readonly=True)
-    employee_guid_1c = fields.Char(string='guid сотрудника 1C', readonly=True)
-    posted = fields.Boolean(string='Проведен?', readonly=True)
-
-    employee_id = fields.Many2one("hr.employee", string="Сотрудник HR", readonly=True)
-
-    @api.depends("employee_id")
-    def _get_name(self):
-        for line in self:
-            if line.employee_id:
-                line.name = line.employee_id.name
 
 
 
 class HrTransferDoc(models.Model):
     _name = "hr.transfer_doc"
+    _inherit = 'hr.base_doc'
     _description = "Переводы сотрудников"
     _order = "date desc"
 
-    name = fields.Char(u'Наименование', compute='_get_name', store=True )
-    date = fields.Date(string='Дата документа', readonly=True)
     start_date = fields.Date(string="Начало")
     end_date = fields.Date(string="Окончание")
-
-    guid_1c = fields.Char(string='guid1C', readonly=True, groups="base.group_erp_manager, base.group_system")
-    number_1c = fields.Char(string='Код 1С', readonly=True)
-    employee_guid_1c = fields.Char(string='guid сотрудника 1C', readonly=True)
-    posted = fields.Boolean(string='Проведен?', readonly=True)
 
     old_job_title = fields.Char(string='Старая должность', compute='_get_old', store=True)
     old_department_id = fields.Many2one("hr.department", string="Старое подразделение", compute='_get_old', store=True)
     
     job_title = fields.Char(string='Должность')
-    department_id = fields.Many2one("hr.department", string="Подразделение")
+    department_guid_1c = fields.Char(string="guid подразделение 1C")
 
-
-    employee_id = fields.Many2one("hr.employee", string="Сотрудник HR", readonly=True)
+    department_id = fields.Many2one("hr.department", string="Подразделение", compute="_get_department", store=True)
 
     is_multi_transfer = fields.Boolean(string='Групповой перевод', readonly=True)
 
-    @api.depends("employee_id")
-    def _get_name(self):
-        for line in self:
-            if line.employee_id:
-                line.name = line.employee_id.name
 
-    @api.depends("job_title", "department_id")
+    def _get_department(self):
+        for line in self:
+            if line.department_guid_1c:
+                dep_search = self.env['hr.department'].search([
+                    ('guid_1c', '=', line.department_guid_1c),
+                    '|',
+                    ('active', '=', False), 
+                    ('active', '=', True)
+                    ],limit=1)
+                line.department_id = dep_search.id
+            else:
+                line.department_id = False
+
+
+    @api.depends("job_title", "department_guid_1c")
     def _get_old(self):
         for line in self:
             old_transfer = self.search([
